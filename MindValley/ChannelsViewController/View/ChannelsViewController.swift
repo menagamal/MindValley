8283fb94c67982dece2a439b3404aafad3180ff5
menagamal <menagamal203@yahoo.com>
2022-04-03 07:39:38 +0200

    base ui design

--- MindValley/ChannelsViewController/View/ChannelsViewController.swift
+++ MindValley/ChannelsViewController/View/ChannelsViewController.swift
@@ -13,5 +13,16 @@ class ChannelsViewController: BaseViewController, ChannelsViewProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
 }
 
